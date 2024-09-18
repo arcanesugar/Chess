7363027a7b2bf403fb7e95e9c1aa55b70dc8f8c8
James <thorntonjamesb@gmail.com>
2024-09-18 14:02:42 +0000

    Added additional position to move generation test suite

--- Search/search.cpp
+++ Search/search.cpp
@@ -367,29 +367,32 @@ void Search::runMoveGenerationTest(Board &board){
 void Search::runMoveGenerationSuite(){
   Board board;
   //https://www.chessprogramming.org/Perft_Results
-  std::string positions[5] = {
+  std::string positions[6] = {
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 ",
     "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ",
     "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ",
     "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8  ",
     "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 "
   };
-  u64 expected[5] = {
+  u64 expected[6] = {
     4865609,
     4085603,
     674624,
     422333,
+    2103487,
     3894594
   };
-  int depths[5] = {
+  int depths[6] = {
     5,
     4,
     5,
+    4,
     4,
     4
   };
   debug::Settings settings;
-  for(int i = 0; i<5; i++){
+  for(int i = 0; i<6; i++){
     board.loadFromFEN(positions[i]);
     u64 found = perftTest(board,depths[i],false);
     std::cout<<"Depth: "<<depths[i];
