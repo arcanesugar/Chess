66f8c9fe7d2a217c67e3c6b112d34bbb984a3fa8
arcanesugar <thornotnjamesb@gmail.com>
2024-11-03 13:48:48 -0700

    Added String struct

--- src/UCI/uci.c
+++ src/UCI/uci.c
@@ -2,10 +2,56 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "../Core/types.h"
 
 #define maxTokens 20
+
+struct String{// captilalized because its a user defined data type
+  char* buf;
+  size_t size;
+  int len;//the number of characters the string can hold (size-1)
+};
+typedef struct String String;
+
+void resizeString(String *str, int newCapacity){
+  int reqBlocks = (newCapacity/8)+1;
+  size_t newSize = reqBlocks*8;
+  str->buf = realloc(str->buf,newSize);
+  str->size = newSize;
+  str->len = newSize-1;
+}
+
+void stringSet(String *str, const char *cstr){
+  if(strlen(cstr)>str->len){
+    resizeString(str,strlen(cstr));
+  }
+  strcpy(str->buf,cstr);
+}
+
+void stringAppend(String *str, const char *cstr){
+  int newSize = strlen(str->buf)+strlen(cstr);
+  if(newSize>str->len){
+    resizeString(str,newSize);
+  }
+  strcat(str->buf,cstr);
+}
+bool stringEqual(String *str, const char *cstr){
+  return (strcmp(str->buf,cstr) == 0);
+}
+String createString(){
+  String str;
+  str.buf = malloc(sizeof(char)*8);
+  str.size = sizeof(char)*8;
+  str.len = str.size-1;
+  stringSet(&str,"");
+  return str;
+};
+
+void destroyString(String *str){
+  free(str->buf);
+}
 
 struct TokenizedString{
   char* string;
@@ -57,27 +103,24 @@ void getToken(TokenizedString ts, int token, char* target){
   target[targetIndex] = '\0';
 }
 
-#define MAX_INPUT_SIZE 255
+void stringFromStdin(String *str){
+  stringSet(str,"");
+  char buf[8];
+  while(fgets(buf,8,stdin) != NULL){
+    stringAppend(str,buf);
+    if(buf[strlen(buf)-1] == '\n') break;//we have reached the end of the line
+  }
+}
+
 void runUCI(){
+  String testString = createString();
   bool quit = false;
   while(!quit){
-    char input[MAX_INPUT_SIZE+1];
-    fgets(input,MAX_INPUT_SIZE,stdin);
-    if(input[strlen(input)-1] == '\n'){
-      input[strlen(input)-1] = '\0';
-    }else{
-      printf("[error]input does not end with newline");
-    }
-    TokenizedString ts = tokenise(input);
-    char token[255];
-    getToken(ts, 0, token);
-    if(strcmp(token,"quit") == 0){
+    stringFromStdin(&testString);
+    printf("%d;%s",testString.len,testString.buf);
+    if(stringEqual(&testString,"quit\n")){
       quit = true;
-      continue;
-    }
-    if(strcmp(token,"uci") == 0){
-      printf("uciok\n");
-      continue;
     }
   }
+  destroyString(&testString);
 }
