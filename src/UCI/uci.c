e2ca9cf005af74667172f31f640b70bed7c67d29
arcanesugar <thornotnjamesb@gmail.com>
2024-11-03 15:43:28 -0700

    Fixed issue with stringAppendChar and added tokenlist

--- src/UCI/uci.c
+++ src/UCI/uci.c
@@ -14,8 +14,8 @@ struct String{// captilalized because its a user defined data type
 typedef struct String String;
 
 void resizeString(String *str, int newCapacity){
-  str->buf = realloc(str->buf,newCapacity+1);
   str->size = sizeof(char)*(newCapacity+1);
+  str->buf = realloc(str->buf,str->size);
   str->capacity = newCapacity;
   str->buf[str->capacity] = '\0';//just in case
 }
@@ -54,12 +54,12 @@ void stringSetChar(String *str, int index, char c){
 
 void stringAppendChar(String *str, char c){
   int index = stringLen(str);
-  if(index>=str->capacity) resizeString(str,index+1);
-  str->buf[index] = c;
+  stringSetChar(str,index,c);
+  stringSetChar(str,index+1,'\0');
 }
 
 char stringGetChar(String *str, int index){
-  if(index>=str->capacity) return 0;
+  if(index>=str->capacity){printf("Index out of range\n"); return 0;}
   return str->buf[index];
 }
 
@@ -69,10 +69,38 @@ String createString(){
   resizeString(&str,8);
   stringSet(&str,"");
   return str;
-};
+}
 
 void destroyString(String *str){
   free(str->buf);
+}
+
+struct TokenList{
+  String tokens[50];
+  int len;
+};
+typedef struct TokenList TokenList;
+
+void createTokenList(String *source, TokenList *tl){
+  tl->len = 0;
+  int tokenIndex = 0;
+  tl->tokens[0] = createString();
+  printf("%s",tl->tokens[0].buf);
+  bool creatingToken = false;
+  for(int i = 0; i<stringLen(source); i++){
+    char c = stringGetChar(source,i);
+    if(c != ' ') creatingToken = true;
+    if(creatingToken){
+      if(c == ' '){
+        tokenIndex++;
+        tl->tokens[tokenIndex] = createString();
+        creatingToken = false;
+      }else{
+        stringAppendChar(&tl->tokens[tokenIndex],c);
+      }
+    }
+  }
+  tl->len = tokenIndex+1;
 }
 
 void stringFromStdin(String *str){
@@ -90,8 +118,13 @@ void runUCI(){
   bool quit = false;
   while(!quit){
     stringFromStdin(&testString);
-    stringSetChar(&testString,7,'l');
-    printf("%d;%s\n",testString.capacity, testString.buf);
+    TokenList tl;
+    createTokenList(&testString,&tl);
+    printf("\ntokens:");
+    for(int i = 0; i<tl.len; i++){
+      printf("%s;", tl.tokens[i].buf);
+    }
+    printf("\ntestString:%s\n",testString.buf);
     if(stringEqual(&testString,"quit")){
       quit = true;
     }
