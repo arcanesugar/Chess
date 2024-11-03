58555180fa8fbec5eed1b56772e971ed8f821fb7
arcanesugar <thornotnjamesb@gmail.com>
2024-11-03 14:26:01 -0700

    Added more string functions

--- src/UCI/uci.c
+++ src/UCI/uci.c
@@ -6,45 +6,67 @@
 
 #include "../Core/types.h"
 
-#define maxTokens 20
-
 struct String{// captilalized because its a user defined data type
   char* buf;
   size_t size;
-  int len;//the number of characters the string can hold (size-1)
+  int capacity;//the number of characters the string can hold (size-1)
 };
 typedef struct String String;
 
 void resizeString(String *str, int newCapacity){
-  int reqBlocks = (newCapacity/8)+1;
-  size_t newSize = reqBlocks*8;
-  str->buf = realloc(str->buf,newSize);
-  str->size = newSize;
-  str->len = newSize-1;
+  str->buf = realloc(str->buf,newCapacity+1);
+  str->size = newCapacity+1;
+  str->capacity = newCapacity;
+}
+
+int stringLen(String *str){
+  return strlen(str->buf);
 }
 
 void stringSet(String *str, const char *cstr){
-  if(strlen(cstr)>str->len){
+  if(strlen(cstr)>str->capacity){
     resizeString(str,strlen(cstr));
   }
   strcpy(str->buf,cstr);
 }
 
+char* stringGetCstr(String *str){
+  return str->buf;
+}
+
 void stringAppend(String *str, const char *cstr){
   int newSize = strlen(str->buf)+strlen(cstr);
-  if(newSize>str->len){
+  if(newSize>str->capacity){
     resizeString(str,newSize);
   }
   strcat(str->buf,cstr);
 }
+
 bool stringEqual(String *str, const char *cstr){
   return (strcmp(str->buf,cstr) == 0);
 }
+
+void stringSetChar(String *str, int index, char c){
+  if(index>=str->capacity) resizeString(str,index+1);
+  str->buf[index] = c;
+}
+
+void stringAppendChar(String *str, char c){
+  int index = stringLen(str);
+  if(index>=str->capacity) resizeString(str,index+1);
+  str->buf[index] = c;
+}
+
+char stringGetChar(String *str, int index){
+  if(index>=str->capacity) return 0;
+  return str->buf[index];
+}
+
 String createString(){
   String str;
   str.buf = malloc(sizeof(char)*8);
   str.size = sizeof(char)*8;
-  str.len = str.size-1;
+  str.capacity = str.size-1;
   stringSet(&str,"");
   return str;
 };
@@ -53,63 +75,14 @@ void destroyString(String *str){
   free(str->buf);
 }
 
-struct TokenizedString{
-  char* string;
-  int numTokens;
-  int tokenStart[maxTokens];
-  int tokenEnd[maxTokens];
-};
-typedef struct TokenizedString TokenizedString;
-
-TokenizedString tokenise(char* string){
-  TokenizedString ts;
-  ts.string = string;
-  bool waitingForTokenStart = true;
-  bool creatingToken = false;
-  ts.numTokens = 0;
-  int tokenIndex = 0;
-  for(int i = 0; i<strlen(string); i++){
-    if(string[i] == ' ' && creatingToken){
-      ts.tokenEnd[tokenIndex] = i;
-      tokenIndex++;
-      ts.numTokens++;
-      waitingForTokenStart = true;
-      creatingToken = false;
-    }
-    if(string[i] != ' ' && waitingForTokenStart){
-      ts.tokenStart[tokenIndex] = i;
-      waitingForTokenStart = false;
-      creatingToken = true;
-    }
-  }
-  if(creatingToken){
-    ts.tokenEnd[tokenIndex] = strlen(string);
-    ts.numTokens++;
-  }
-  return ts;
-}
-
-void getToken(TokenizedString ts, int token, char* target){
-  if(token>ts.numTokens){
-    printf("token index out of range");
-  }
-  int strIndex = ts.tokenStart[token];
-  int targetIndex = 0;
-  while(strIndex<ts.tokenEnd[token]){
-    target[targetIndex] = ts.string[strIndex];
-    targetIndex++;
-    strIndex++;
-  }
-  target[targetIndex] = '\0';
-}
-
 void stringFromStdin(String *str){
   stringSet(str,"");
   char buf[8];
   while(fgets(buf,8,stdin) != NULL){
     stringAppend(str,buf);
-    if(buf[strlen(buf)-1] == '\n') break;//we have reached the end of the line
+    if(buf[strlen(buf)-1] == '\n') break;
   }
+  stringSetChar(str,stringLen(str)-1,'\0');//remove newline character
 }
 
 void runUCI(){
@@ -117,8 +90,8 @@ void runUCI(){
   bool quit = false;
   while(!quit){
     stringFromStdin(&testString);
-    printf("%d;%s",testString.len,testString.buf);
-    if(stringEqual(&testString,"quit\n")){
+    stringSetChar(&testString,7,'l');
+    if(stringEqual(&testString,"quit")){
       quit = true;
     }
   }
