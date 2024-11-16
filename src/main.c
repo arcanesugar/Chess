152f9f575d0e4777314691aa3c54b29d57ee8efe
arcanesugar <thornotnjamesb@gmail.com>
2024-11-16 13:45:26 -0700

    Cleaned up main

--- src/main.c
+++ src/main.c
@@ -7,14 +7,10 @@
 int main(int argc, char *argv[]) {
   initMoveGenerator();
   initEval();
-  if(argc>1){//argc[0] is the name of the program, so theres always at least 1 argument
-    if(strcmp(argv[1],"uci") == 0){
-      runUCI();
-      cleanupMoveGenerator();
-      return 0;
-    }
-  }
-  runConsoleInterface(STARTPOS_FEN);
+  if(argc>1 && strcmp(argv[1],"uci") == 0)//argc[0] is the name of the program, so theres always at least 1 argument
+    runUCI();
+  else
+    runConsoleInterface(STARTPOS_FEN);
   cleanupMoveGenerator();
   return 0;
 }
